23d2c5292ade6caf68cf1ee7837a21e4af4e6cd4
Hiroshi Usui <usui@berkeley.edu>
2021-07-23 14:22:41 -0700

    fix the object syntax error to callback fn

--- client/src/components/canvas.js
+++ client/src/components/canvas.js
@@ -1,42 +1,44 @@
 // import * as actions from "actions/item"
-import * as paper from "paper"
-import React, { useEffect } from "react"
-import { useSelector } from "react-redux"
-import { useActions } from "react-redux-actions-hook"
-import { useHistory } from "react-router-dom"
-import { io } from "socket.io-client"
+import * as paper from "paper";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useActions } from "react-redux-actions-hook";
+import { useHistory } from "react-router-dom";
+import { io } from "socket.io-client";
 
 export default () => {
   // const action = useActions(actions)
-  const history = useHistory()
+  const history = useHistory();
   // const { auth, item } = useSelector((state) => ({
   //   item: state.item,
   // }));
 
   useEffect(() => {
-    const canvas = document.getElementById("myCanvas")
-    paper.setup(canvas)
-    const tool = new paper.Tool()
-    const path = new paper.Path()
-    const uid = Date.now() % 10000
-    path.strokeColor = "red"
+    const canvas = document.getElementById("myCanvas");
+    paper.setup(canvas);
+    const tool = new paper.Tool();
+    const path = new paper.Path();
+    const uid = Date.now() % 10000;
+    path.strokeColor = "red";
 
-    const ws = io(new URL("/", window.location.href).href.replace("http", "ws"))
+    const ws = io(
+      new URL("/", window.location.href).href.replace("http", "ws")
+    );
 
     tool.onMouseMove = function (event) {
       //http://paperjs.org/reference/tool/ path examples
-      path.add(event.point)
+      path.add(event.point);
       ws.emit(
         "mouseMove",
         JSON.stringify({ x: event.point.x, y: event.point.y, uid })
-      )
-    }
-    ws.on("mouseMove", (data) => {})
+      );
+    };
+    ws.on("mouseMove", (data) => {});
 
-    ws.on("disconnect", {
-      console.error("Chat socket closed unexpectedly")
+    ws.on("disconnect", () => {
+      console.error("Chat socket closed unexpectedly");
     });
-  }, [])
+  }, []);
 
-  return <canvas className="canvas"></canvas>
-}
+  return <canvas className="canvas"></canvas>;
+};
