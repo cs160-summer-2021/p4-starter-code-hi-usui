bbe7b005056879422c018494fe46226b9c4ac28a
Hiroshi Usui <usui@berkeley.edu>
2021-07-23 17:24:24 -0700

    almost done, just need to sync disconnected users' point histories

--- client/src/components/canvas.js
+++ client/src/components/canvas.js
@@ -17,26 +17,42 @@ export default () => {
     const canvas = document.getElementById("myCanvas");
     paper.setup(canvas);
     const tool = new paper.Tool();
-    const path = new paper.Path();
-    const uid = Date.now() % 10000;
-    path.strokeColor = "red";
 
     const ws = io(
       new URL("/", window.location.href).href.replace("http", "ws")
     );
 
-    tool.onMouseMove = function (event) {
-      //http://paperjs.org/reference/tool/ path examples
-      path.add(event.point);
-      ws.emit(
-        "mouseMove",
-        JSON.stringify({ x: event.point.x, y: event.point.y, uid })
-      );
-    };
-    ws.on("mouseMove", (data) => {});
+    const allClients = {};
 
-    ws.on("disconnect", () => {
-      console.error("Chat socket closed unexpectedly");
+    ws.on("connectClient", (data) => {
+      console.log(`Client '${data.id}' has connected!}`);
+      allClients[data.id] = { path: new paper.Path() };
+      for (let key in data.payload.pathProperties) {
+        allClients[data.id].path[key] = data.payload.pathProperties[key];
+      }
+      for (let point of data.payload.points) {
+        allClients[data.id].path.add(point);
+      }
+      console.log(data.payload);
+    });
+
+    ws.on("disconnectClient", (id) => {
+      console.log(`Client '${id}' has disconnected!`);
+      delete allClients[id];
+    });
+
+    tool.onMouseMove = (event) => {
+      const { x, y } = event.point;
+      ws.emit("onMouseMove", { id: ws.id, payload: { x, y } });
+    };
+
+    ws.on("addPoint", (data) => {
+      if (data.id) {
+        const { x, y } = data.payload;
+        if (allClients[data.id]) {
+          allClients[data.id].path.add({ x, y });
+        }
+      }
     });
   }, []);
 
--- server/index.js
+++ server/index.js
@@ -10,18 +10,61 @@ const io = new Server(server)
 
 const connections = {}
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min) + min) // The maximum is exclusive and the minimum is inclusive
+}
+
+const randomColorHex = () => {
+  const validCharacters = "ABCDEF0123456789".split("")
+  let hexColor = "#"
+  for (let i = 0; i < 6; i++) {
+    hexColor += validCharacters[getRandomInt(0, 16)]
+  }
+  return hexColor
+}
+
 io.on("connection", async (socket) => {
   const id = socket.id
   console.log(`Client id '${id}' connected!`)
-  if (!connections[id]) {
-    connections[id] = id
+
+  connections[id] = {
+    id,
+    socket,
+    payload: {
+      pathProperties: { strokeColor: randomColorHex() },
+      points: [],
+    },
   }
-  socket.on("mouseMove", async (data) => {
-    console.log(data)
+  for (let anyId in connections) {
+    connections[anyId].socket.emit("connectClient", {
+      id,
+      payload: connections[id].payload,
+    })
+    connections[id].socket.emit("connectClient", {
+      id: anyId,
+      payload: connections[anyId].payload,
+    })
+    for (let point of connections[anyId].payload.points) {
+      connections[id].socket.emit("addPoint", { id, payload: point })
+    }
+  }
+
+  socket.on("onMouseMove", async (data) => {
+    connections[data.id].payload.points.push(data.payload)
+    for (let anyId in connections) {
+      connections[anyId].socket.emit("addPoint", { id, payload: data.payload })
+    }
   })
 
-  socket.on("disconnect", (reason) => {
+  socket.on("disconnect", () => {
+    console.log(`Client '${id}' disconnected!`)
     delete connections[id]
+    for (let anyId in connections) {
+      connections[anyId].socket.emit("disconnectClient", id)
+    }
   })
 })
 app.use((req, res, next) => {
