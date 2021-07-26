9721f2de8677f55d9aa4095e5e2b8d54c43d8068
Samuel Stulman <samuel.stulman@berkeley.edu>
2021-07-26 10:11:16 -0700

    commiting merge conflict

---
+++ client/src/reducers/index.js
@@ -0,0 +1,8 @@
+// import errorsReducer from "reducers/errorsReducer";
+// import eventsReducer from "reducers/eventsReducer";
+import { combineReducers } from "redux"
+
+export default combineReducers({
+  // errors: errorsReducer,
+  // events: eventsReducer,
+})
