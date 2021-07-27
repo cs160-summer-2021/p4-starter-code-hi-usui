96a3db460be0e9e53fc7d7f600be43cdc3066202
Hiroshi Usui <usui@berkeley.edu>
2021-07-27 16:21:17 -0700

    make QR code size dynamic

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,67 +1,67 @@
 // https://dev.to/bravemaster619/how-to-use-socket-io-client-correctly-in-react-app-o65
 //  How to use socket.io-client correctly in React app
-import { PLAYLIST_SET, PLAYLIST_USER_CONNECT } from "actions/types"
-import axios from "axios"
-import Spinner from "components/common/Spinner"
-import { SocketContext } from "context/socket"
-import { useWindowDimensions } from "helpers/_index"
-import React, { useContext, useEffect, useState } from "react"
-import QRCode from "react-qr-code"
-import { useDispatch, useSelector } from "react-redux"
-import { useActions } from "react-redux-actions-hook"
-import { useHistory, useLocation } from "react-router-dom"
+import { PLAYLIST_SET, PLAYLIST_USER_CONNECT } from "actions/types";
+import axios from "axios";
+import Spinner from "components/common/Spinner";
+import { SocketContext } from "context/socket";
+import { useWindowDimensions } from "helpers/_index";
+import React, { useContext, useEffect, useState } from "react";
+import QRCode from "react-qr-code";
+import { useDispatch, useSelector } from "react-redux";
+import { useActions } from "react-redux-actions-hook";
+import { useHistory, useLocation } from "react-router-dom";
 
-import "../sass/Landing.scss"
+import "../sass/Landing.scss";
 
-const useQuery = () => new URLSearchParams(useLocation().search)
+const useQuery = () => new URLSearchParams(useLocation().search);
 
 export default () => {
-  const socket = useContext(SocketContext)
-  const dispatch = useDispatch()
-  const history = useHistory()
-  const query = useQuery()
-  const size = query.get("size")
-  const { height, width } = useWindowDimensions()
+  const socket = useContext(SocketContext);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const query = useQuery();
+  const { height, width } = useWindowDimensions();
   const { playlist } = useSelector((state) => ({
     playlist: state.playlist,
-  }))
+  }));
 
   useEffect(() => {
     (async () => {
-      const res = await axios.get("/api/playlists/new")
-      const playlistId = res.data._id
-      dispatch({ type: PLAYLIST_SET, payload: playlistId })
-    })()
+      const res = await axios.get("/api/playlists/new");
+      const playlistId = res.data._id;
+      dispatch({ type: PLAYLIST_SET, payload: playlistId });
+    })();
 
     socket.on("clientConnect", (data) => {
       dispatch({
         type: PLAYLIST_USER_CONNECT,
         payload: { id: data.id, device: data.payload.device },
-      })
-    })
-  }, [])
+      });
+    });
+  }, []);
 
   const users = () => {
     if (
       playlist.playlistId &&
       playlist.users.length &&
       playlist.users.some((u) => u.device == "phone")
-      
     ) {
       setTimeout(() => {
-        history.push(`/playlists/${playlist.playlistId}/display`)
-      }, 5000)
+        history.push(`/playlists/${playlist.playlistId}/display`);
+      }, 5000);
       return (
         <div style={{ color: "red" }}>
           Users have joined your playlist! Redirecting to playlist in 5
           seconds....
           <Spinner />
         </div>
-      )
+      );
     }
-  }
+  };
 
   const qr = () => {
+    const size = query.get("size");
+    console.log(size);
     if (playlist.playlistId) {
       return (
         <a href={`/playlists/${playlist.playlistId}/phone`}>
@@ -69,11 +69,12 @@ export default () => {
             value={`${new URL("/", window.location.href)}playlists/${
               playlist.playlistId
             }/phone`}
+            size={size ? size : 256}
           />
         </a>
-      )
+      );
     }
-  }
+  };
 
   return (
     <div className="container-fluid">
@@ -87,9 +88,7 @@ export default () => {
           {qr()}
           <h3>Click on QR code if scanning unavailable.</h3>
         </div>
-        {/* <div className="col-sm-6">
-          {users()}
-        </div> */}
+        <div className="col-sm-6">{users()}</div>
       </div>
       <div className="row height-width">
         <div className="col-sm-12">
@@ -99,5 +98,5 @@ export default () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
